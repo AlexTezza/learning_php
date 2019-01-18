45ebb02dbfeeef725137ece2c48b71660e1b428b
alex.tezza <tezzaalex@gmail.com>
2019-01-18 10:28:19 -0200

    Aprendendo PHP - Strings - Exemplo_01

---
+++ strings/exemplo-01.php
@@ -0,0 +1,19 @@
+<?php
+
+// Diferença entre Aspas Duplas e Simples.
+
+$nome = "Hcode";
+
+$nome2 = 'Treinamentos';
+
+var_dump($nome, $nome2);
+
+echo "<br>";
+
+echo "ABC $nome"; // Concatena a String com a variável
+
+echo "<br>";
+
+echo 'ABC $nome2'; // Não reconhece a variável
+
+?>
