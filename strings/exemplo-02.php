dbe7bf84edc247156272d0a8a7755ab84a4c1a9c
alex.tezza <tezzaalex@gmail.com>
2019-01-18 10:28:29 -0200

    Aprendendo PHP - Strings - Exemplo_02

---
+++ strings/exemplo-02.php
@@ -0,0 +1,21 @@
+<?php
+
+// Manipulação de Maiúsculo e Minúsculo em String
+
+$nome = "alex tezza";
+
+echo strtolower($nome);
+
+echo "<br>";
+
+echo ucfirst($nome);
+
+echo "<br>";
+
+echo ucwords($nome);
+
+echo "<br>";
+
+echo strtoupper($nome);
+
+?>
