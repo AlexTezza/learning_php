7807271ecc6e6410c9be2e2591b9ebbd03fe1bc8
Alex Tezza <tezzaalex@gmail.com>
2019-02-05 22:32:20 -0200

    Aprendendo PHP - Date

---
+++ date/exemplo-01.php
@@ -0,0 +1,11 @@
+<?php
+
+// http://php.net/manual/pt_BR/function.date.php
+
+echo date("d/m/Y H:i:s", 1549411593);
+
+echo '<br>';
+
+echo time();
+
+?>
---
+++ date/exemplo-02.php
@@ -0,0 +1,14 @@
+<?php
+
+// $ts = strtotime("now");
+// $ts = strtotime("now +1");
+// $ts = strtotime("now +1 week");
+$ts = strtotime("2001-09-11");
+
+echo $ts;
+
+echo "<br>";
+
+echo date("l, d/m/Y", $ts)
+
+?>
