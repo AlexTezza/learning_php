63466bcd525839f4ea46b704b2350d9760ae7d55
Alex Tezza <tezzaalex@gmail.com>
2019-02-25 20:26:41 -0300

    Aprendendo PHP - Try/Catch/Finally, exemplo de tratamento de erros.

---
+++ try/exemplo-02.php
@@ -0,0 +1,21 @@
+<?php
+
+function trataNome($nome) {
+    if (!$nome) {
+        throw new Exception("Nenhum nome foi informado.", 1);
+    }
+
+    echo ucfirst($nome). "<br>";
+}
+
+try {
+    trataNome("Alex");
+    trataNome("");
+} catch(Exception $e) {
+    echo $e->getMessage();
+} finally {
+    echo "<br>";
+    echo "Executou o Try";
+}
+
+?>
