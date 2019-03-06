91cf4e52ac0faa809739f8260d0296077b4f963f
Alex Tezza <tezzaalex@gmail.com>
2019-03-05 21:48:43 -0300

    Aprendendo PHP - reCaptcha, exemplo de utilização do reCaptha.

---
+++ security/exemplo-04.php
@@ -0,0 +1,9 @@
+<?php
+?>
+
+<script src="https://www.google.com/recaptcha/api.js" async defer></script>
+<form action="cadastrar.php" method="post">
+    <input type="email" name="inputEmail">
+    <div class="g-recaptcha" data-sitekey="6LeUu5UUAAAAABBG9gEsbSCnuR9yUM6zdQ1S_Uef"></div>
+    <button type="submit">Enviar</button>
+</form>
